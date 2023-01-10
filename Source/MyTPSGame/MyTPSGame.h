a16e2c4ef7922d9e547f1c8e43323a2c98a99477
oh-bba-ya <audtjrauddn@khu.ac.kr>
2023-01-10 15:40:01 +0900

    Add : Macro UE_LOG
    Macro를 통해 UE_LOG를 출력해보자.

--- Source/MyTPSGame/MyTPSGame.h
+++ Source/MyTPSGame/MyTPSGame.h
@@ -4,3 +4,10 @@
 
 #include "CoreMinimal.h"
 
+
+#define CALLINFO (FString(__FUNCTION__) + TEXT("(") + FString::FromInt(__LINE__) + TEXT(")"))
+
+#define PRINT_CALLINFO() UE_LOG(LogTemp, Warning, TEXT("%s"), *CALLINFO);
+
+// ##__VA_ARGS__ : 가변인자 매크로
+#define PRINT_LOG(fmt, ...) UE_LOG(LogTemp, Warning, TEXT("%s %s"), *CALLINFO, *FString::Printf(fmt, ##__VA_ARGS__));
--- Source/MyTPSGame/MyTPSGameGameModeBase.cpp
+++ Source/MyTPSGame/MyTPSGameGameModeBase.cpp
@@ -2,10 +2,12 @@
 
 
 #include "MyTPSGameGameModeBase.h"
-
+#include "MyTPSGame.h"
 AMyTPSGameGameModeBase::AMyTPSGameGameModeBase() {
 	// 생성자의 경우 언리얼 프로젝트를 시작할때 (에디터 실행을 뜻하는게 아님) 생성된다.
 
 	// 로그 출력
-	UE_LOG(LogTemp, Warning, TEXT("Hello World!"));
+	//UE_LOG(LogTemp, Warning, TEXT("Hello World!"));
+
+	PRINT_LOG(TEXT("%s %s"), TEXT("Hello"), TEXT("World"));
 }
