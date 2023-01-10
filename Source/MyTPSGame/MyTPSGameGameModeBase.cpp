217400fb6adc25b4b6392fb3397441505491450f
oh-bba-ya <audtjrauddn@khu.ac.kr>
2023-01-10 14:37:22 +0900

    Initial commit

---
+++ Source/MyTPSGame/MyTPSGameGameModeBase.cpp
@@ -0,0 +1,11 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "MyTPSGameGameModeBase.h"
+
+AMyTPSGameGameModeBase::AMyTPSGameGameModeBase() {
+	// 생성자의 경우 언리얼 프로젝트를 시작할때 (에디터 실행을 뜻하는게 아님) 생성된다.
+
+	// 로그 출력
+	UE_LOG(LogTemp, Warning, TEXT("Hello World!"));
+}
